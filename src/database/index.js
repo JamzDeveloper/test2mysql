5e03e5853bb175f95a8580013fe23c0f63bddad3
jmontenegro <jamzdeveloper@gmail.com>
2024-05-18 18:11:09 -0500

    added databaseHost .env

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
-  host: "localhost",
+  host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
